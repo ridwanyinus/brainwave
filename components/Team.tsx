dc429ea61ae2616ee616d7f24cdfeb8c87c1abb1
Ridwan <ridwanyinusa15@gmail.com>
2024-07-31 15:23:18 +0100

    added changes

--- components/Team.tsx
+++ components/Team.tsx
@@ -13,11 +13,15 @@ import team8 from "@/public/team8.png";
 
 const Team = () => {
   return (
-    <main className="py-28 2xl:py-32 px-5 lg:px-[1.9rem]x l:px-20 desktop:px-28 2xl:px-48 largesceen:px-80 fourk:px-[40rem]  flex flex-col lg:flex-row justify-center gap-x-36 desktop:gap-x-56 2xl:gap-x-72 largesceen:gap-x-96 items-center mt-20 ">
+    <main className="py-16 lg:py-28 2xl:py-32 px-5 lg:px-[1.9rem] xl:px-20 desktop:px-28 2xl:px-48 largesceen:px-80 fourk:px-[40rem]  flex flex-col-reverse lg:flex-row justify-center lg:gap-x-44 xl:gap-x-56 2xl:gap-x-72 largesceen:gap-x-96 items-center   xl:mt-20 ">
       <section className="relative">
-        <Image src={rectangle} alt="image of team" className="rounded-[0.625rem] w-[15.5rem] largesceen:w-[20.5rem] h-auto object-cover relative z-10" />
+        <Image src={rectangle} alt="image of team" className="rounded-[0.625rem] w-[9rem] xs:w-[10rem] sm:w-[12rem] xl:w-[15.5rem] largesceen:w-[20.5rem] h-auto object-cover relative z-10" />
 
-        <Image src={dots} alt="dots" className="absolute -left-12 largesceen:-left-14 bottom-16 largesceen:bottom-24 largesceen:h-[9rem] w-auto   largesceen:object-fill bg-no-repeat" />
+        <Image
+          src={dots}
+          alt="dots"
+          className="absolute -left-8 xl:-left-12 largesceen:-left-14 bottom-10 sm:bottom-16 xl:bottom-20 largesceen:bottom-24 h-[4.5rem] xs:h-[5rem] xl:h-[7rem] largesceen:h-[9rem] w-auto largesceen:object-fill bg-no-repeat"
+        />
 
         <div className="team_rectangle w-[13.375rem] largesceen:w-[17rem] h-[11.25rem] largesceen:h-[13rem] absolute -bottom-20 -right-12"></div>
 
@@ -25,40 +29,40 @@ const Team = () => {
           <Image
             src={team2}
             alt="team members"
-            className="team-bs absolute rounded-[0.625rem] -left-[5.5rem] largesceen:-left-[7.5rem] -top-8 largesceen:-top-12 w-[4.438rem] largesceen:w-[6rem] h-auto"
+            className="team-bs absolute rounded-[0.625rem] -left-14 sm:-left-[4.2rem] xl:-left-[5.5rem] largesceen:-left-[7.5rem] -top-6 sm:-top-8 largesceen:-top-12 w-[3rem] sm:w-[3.438rem] xl:w-[4.438rem] largesceen:w-[6rem] h-auto"
           />
 
-          <Image src={team3} alt="team members" className="team-bs absolute rounded-[0.625rem] right-20 largesceen:right-28 -top-28 largesceen:-top-36 w-[5.5rem] largesceen:w-[7.438rem] h-auto" />
+          <Image src={team3} alt="team members" className="team-bs absolute rounded-[0.625rem] right-16 xl:right-20 largesceen:right-28 -top-[4.5rem] sm:-top-20 xl:-top-28 largesceen:-top-36 w-14 sm:w-16 xl:w-[5.5rem] largesceen:w-[7.438rem] h-auto" />
 
           <Image
             src={team4}
             alt="team members"
-            className="team-bs absolute rounded-[0.625rem] -right-[6rem] largesceen:-right-[8.5rem] -top-8 largesceen:-top-14 w-[5rem] largesceen:w-[7rem] h-auto"
+            className="team-bs absolute rounded-[0.625rem] -right-16 sm:-right-[4.7rem] xl:-right-[6rem] largesceen:-right-[8.5rem] -top-8 largesceen:-top-14 w-[3rem] sm:w-[3.8rem] xl:w-[5rem] largesceen:w-[7rem] h-auto"
           />
 
           <Image
             src={team5}
             alt="team members"
-            className="team-bs absolute rounded-[0.625rem] -right-[5.8rem] largesceen:-right-[8rem] top-32 largesceen:top-44 w-[4.438rem] largesceen:w-[6rem] h-auto"
+            className="team-bs absolute rounded-[0.625rem] -right-16 sm:-right-[4.7rem] xl:-right-[5.8rem] largesceen:-right-[8rem] top-20 sm:top-32 largesceen:top-44 w-[3rem] sm:w-[3.438rem] xl:w-[4.438rem] largesceen:w-[6rem] h-auto"
           />
 
-          <Image src={team6} alt="team members" className="team-bs absolute rounded-[0.625rem] right-[2rem] -bottom-10 w-[4.313rem] largesceen:w-[5.5rem] h-auto z-10" />
+          <Image src={team6} alt="team members" className="team-bs absolute rounded-[0.625rem] right-4 sm:right-5 xl:right-[2rem] -bottom-8 sm:-bottom-6 xl:-bottom-10 w-[3rem] sm:w-[3.313rem] xl:w-[4.313rem] largesceen:w-[5.5rem] h-auto z-10" />
 
           <Image
             src={team7}
             alt="team members"
-            className="team-bs absolute rounded-[0.625rem] right-36 largesceen:right-44 -bottom-[7.5rem] largesceen:-bottom-40 w-[5rem] largesceen:w-[7rem] h-auto "
+            className="team-bs absolute rounded-[0.625rem] right-24 sm:right-28 xl:right-36 largesceen:right-44 -bottom-20 lg:-bottom-[5.5rem] xl:-bottom-[7.5rem] largesceen:-bottom-40 w-[3rem] sm:w-[3.8rem] xl:w-[5rem] largesceen:w-[7rem] h-auto "
           />
 
           <Image
             src={team8}
             alt="team members"
-            className="team-bs absolute rounded-[0.625rem] -left-28 largesceen:-left-40 -bottom-10 largesceen:-bottom-12 w-[5.938rem] largesceen:w-[8rem] h-auto "
+            className="team-bs absolute rounded-[0.625rem] -left-[4.5rem] sm:-left-24 xl:-left-28 largesceen:-left-40 -bottom-8 sm:-bottom-10 largesceen:-bottom-12 w-[3.9rem] sm:w-[4.938rem] xl:w-[5.938rem] largesceen:w-[8rem] h-auto "
           />
         </div>
       </section>
 
-      <section>
+      <section className="max-lg:pb-32">
         <h1 className="text-dark text-3xl xl:text-4xl 2xl:text-5xl largesceen:text-6xl font-bold w-[15rem] xl:w-[20rem] 2xl:w-[23.438rem] largesceen:w-[29rem] tracking-[-1.8px] leading-[120.833%] mb-2 lg:mb-4 fourk:mb-6">
           Collaborate with team members.
         </h1>
