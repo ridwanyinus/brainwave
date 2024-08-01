d76a86193d3604db91fd2c16e6428492f5163da5
Ridwan <ridwanyinusa15@gmail.com>
2024-08-01 12:31:21 +0100

    added changes

--- components/Team.tsx
+++ components/Team.tsx
@@ -11,6 +11,10 @@ import team7 from "@/public/team7.png";
 import team8 from "@/public/team8.png";
 import app from "@/public/App 02.png";
 import app_content from "@/public/app-content.png";
+import app_contect_dot from "@/public/Group 101.svg";
+import oval_team1 from "@/public/Ovalteam1.svg";
+import oval_team2 from "@/public/Ovalteam2.svg";
+import oval_team3 from "@/public/Ovalteam3.svg";
 const Team = () => {
   return (
     <main className="py-16 lg:py-20 2xl:py-32 px-5 lg:px-[1.9rem] xl:px-20 desktop:px-28 2xl:px-48 largesceen:px-80 fourk:px-[40rem] xl:mt-20">
@@ -91,9 +95,9 @@ const Team = () => {
         </section>
       </div>
 
-      <section className="py-16 lg:py-20 2xl:py-32  xl:mt-20 largesceen:mt-28 flex flex-col-reverse lg:flex-row justify-center lg:gap-x-44 xl:gap-x-56 2xl:gap-x-72 largesceen:gap-x-96 items-center">
-        <section className="max-lg:pb-32">
-          <h1 className="text-dark text-3xl xl:text-4xl 2xl:text-5xl largesceen:text-6xl font-bold w-[15rem] xl:w-[20rem] 2xl:w-[23.438rem] largesceen:w-[29rem] tracking-[-1.8px] leading-[120.833%] mb-2 lg:mb-4 fourk:mb-6">
+      <section className="py-16 lg:py-20 2xl:py-32  xl:mt-20 largesceen:mt-28 flex flex-col lg:flex-row justify-center lg:gap-x-44 xl:gap-x-56 2xl:gap-x-72 largesceen:gap-x-96 items-center">
+        <section className="max-lg:py-16">
+          <h1 className="text-dark text-3xl xl:text-4xl 2xl:text-5xl largesceen:text-6xl font-bold w-[15rem] xl:w-[20rem] 2xl:w-[23.438rem] largesceen:w-[32rem] tracking-[-1.8px] leading-[120.833%] mb-2 lg:mb-4 fourk:mb-6">
             Organize remote team fast & easily.
           </h1>
           <p className="w-[16rem] md:w-[20rem]  xl:w-[24rem] 2xl:w-[23.438rem]  text-dark text-opacity-[0.7] font-normal tracking-[-0.2px]  leading-[168.421%] text-sm xl:text-base  desktop:text-lg largesceen:text-xl fourk:text-2xl mb-4 lg:mb-6 2xl:mb-8 fourk:mb-10">
@@ -111,9 +115,26 @@ const Team = () => {
           </p>
         </section>
 
-        <section className="relative">
-          <Image src={app} alt="app" className="rounded-lg sm:rounded-[0.938rem] app2 h-[31.375rem] largesceen:h-full w-[16.125rem] largesceen:w-[17.5rem]" />
-          <Image src={app_content} alt="app" className="absolute -left-[10rem] top-24 app2-content w-[12.313rem] largesceen:w-[13rem]" />
+        <section className="relative mt-2 sm:mt-4 lg:mt-0">
+          <Image src={app} alt="app" className="rounded-lg sm:rounded-[0.938rem] app2 h-[22rem] sm:h-[28rem] desktop:h-[31.375rem]  largesceen:h-full w-[14rem] xs:w-[16.125rem] largesceen:w-[17.5rem] z-10 relative" />
+          <Image src={app_content} alt="app" className="absolute -left-8 xs:-left-10 sm:-left-28 lg:-left-[10rem] top-12 desktop:top-20 largesceen:top-24 app2-content w-[8rem] lg:w-[12.313rem] largesceen:w-[13rem] z-20" />
+
+          <div className="z-20 app-content_general w-[7rem] sm:w-[8rem] lg:w-[10rem] largesceen:w-[11.875rem] h-[7rem] sm:h-[8rem] lg:h-[10rem] largesceen:h-[11.875rem] absolute bottom-12 sm:bottom-28 desktop:bottom-32 largesceen:bottom-[10.3rem] -left-10 sm:-left-16 flex flex-col justify-center items-center">
+            <div className="flex relative justify-center items-center">
+              <Image src={oval_team1} alt="team " className="translate-x-2 lg:translate-x-4 w-8 lg:w-auto" />
+              <Image src={oval_team2} alt="team" className="z-10 w-8 lg:w-auto " />
+              <Image src={oval_team3} alt="team" className="-translate-x-2 lg:-translate-x-4 w-8 lg:w-auto" />
+            </div>
+
+            <h3 className="text-white text-base lg:text-lg 2xl:text-[1.188rem] font-bold tracking-[-0.2px] mt-4">General</h3>
+
+            <p className="text-white text-opacity-[0.7056] font-medium text-xs lg:text-sm">46 Members</p>
+          </div>
+
+          <div>
+            <Image src={app_contect_dot} alt="dots" className="max-xs:hidden absolute bottom-4 sm:bottom-20 lg:bottom-10 desktop:bottom-20 largesceen:bottom-32 -left-[5.2rem] sm:-left-[7.5rem] h-[3rem] sm:h-[4rem] lg:h-[8.688rem] desktop:h-auto" />
+            <div className="app-content_bg absolute -top-4 sm:-top-8 -right-5 sm:-right-8 h-[13.5rem] largesceen:h-60 w-[10.938rem] largesceen:w-48 "></div>
+          </div>
         </section>
       </section>
     </main>
